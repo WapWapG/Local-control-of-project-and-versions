c8e2151650637167f9cbedb77db3713a82d2623d
WapWapG <kirillvital9@gmail.com>
2026-03-24 19:42:24 +0700

    Add files via upload
    Добавление main.cpp, начинаю писать код

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+//Функция приветсвия пользователя, вывод первого сообщения.
+void hello(){
+    cout <<  "Привет! что будешь делать?" <<"\n";
+    cout <<  "1. Мои проекты(В реализции)";
+}
+
+int databaseCheck(int number){
+    if (number == 1){
+        // реализация, взятие переменных из database.cpp в зависимости от числа
+    }
+}
+
+int main(){
+    //Инициализация переменных
+    //и вывод списка действий
+    int answer;
+    int number;
+    hello();
+    cin >> answer;
+    if (answer == 1){
+        number == 1;
+        databaseCheck(number);
+        //На доработку
+    }
+}
